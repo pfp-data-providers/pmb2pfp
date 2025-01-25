52d37c0805b0e01184714c4f63e8cf93c3841589
csae8092 <p.andorfer@gmail.com>
2025-01-25 09:51:27 +0100

    validate with java thing

---
+++ validate.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+SHACL=datasets/myshapes.ttl
+rm ${SHACL}
+curl -o ${SHACL} https://pfp-schema.acdh-ch-dev.oeaw.ac.at/shacl/shacl.ttl
+docker run --rm -v ${PWD}/datasets:/data ghcr.io/ashleycaselli/shacl:latest validate -datafile /data/pmb.ttl -shapesfile /data/myshapes.ttl > validation_report.txt
