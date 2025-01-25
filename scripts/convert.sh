3fef611dbd02a9aa52f0cb0d5f674dd1d86ef1da
csae8092 <p.andorfer@gmail.com>
2025-01-25 16:06:27 +0100

    minor

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Converting pmb.nt to pmb.ttl"
-docker run --rm -v ${PWD}/datasets:/rdf stain/jena riot pmb.nt > ${PWD}/datasets/pmb.ttl
+docker run --rm -v ${PWD}/datasets:/rdf stain/jena riot --output=TURTLE pmb.nt > ${PWD}/datasets/pmb.ttl
