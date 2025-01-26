3dadde3b8ea71775395c5eafc161102b7dc67ded
csae8092 <p.andorfer@gmail.com>
2025-01-26 16:37:57 +0100

    feat: add execution time to PMB dataset creation message

--- scripts/build_graph.sh
+++ scripts/build_graph.sh
@@ -1,9 +1,16 @@
+start_time=$(date +%s)
+
 python scripts/pmb_orgs.py
 python scripts/pmb_places.py
 python scripts/pmb_persons.py
 python scripts/pmb_person_person_relations.py
 python scripts/pmb.py
 
+end_time=$(date +%s)
+duration=$((end_time - start_time))
+formatted_duration=$(printf '%02dh:%02dm:%02ds\n' $(($duration/3600)) $(($duration%3600/60)) $(($duration%60)))
+
 numberoflines=$(wc -l < datasets/pmb.nt)
 formatted_number=$(printf "%'d" $numberoflines)
-echo "PMB Dataset with ${formatted_number} triples"
+
+echo "Created PMB Dataset with ${formatted_number} triples in ${formatted_duration}"
