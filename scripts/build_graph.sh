939d7619ed0360d056a918aaa8cd7bf856eef5d4
csae8092 <p.andorfer@gmail.com>
2025-01-26 16:34:06 +0100

    feat: echo number of triples

--- scripts/build_graph.sh
+++ scripts/build_graph.sh
@@ -3,3 +3,7 @@ python scripts/pmb_places.py
 python scripts/pmb_persons.py
 python scripts/pmb_person_person_relations.py
 python scripts/pmb.py
+
+numberoflines=$(wc -l < datasets/pmb.nt)
+formatted_number=$(printf "%'d" $numberoflines)
+echo "PMB Dataset with ${formatted_number} triples"
