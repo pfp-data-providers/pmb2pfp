fe309ae64d3d91b78031f2237b36a2e366edec88
csae8092 <p.andorfer@gmail.com>
2025-01-27 10:15:18 +0100

    feat: adds some logging to conversion script

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+start_time=$(date +%s)
+
+DATA_DIR=datasets
+NT=pmb.nt
+TTL=pmb.ttl
 
 echo "Converting pmb.nt to pmb.ttl"
-docker run --rm -v ${PWD}/datasets:/rdf stain/jena riot --output=TURTLE pmb.nt > ${PWD}/datasets/pmb.ttl
+
+NT_SIZE=$(du -h ${DATA_DIR}/${NT})
+docker run --rm -v ${PWD}/datasets:/rdf stain/jena riot --output=TURTLE ${NT} > ${PWD}/${DATA_DIR}/${TTL}
+TTL_SIZE=$(du -h ${DATA_DIR}/${TTL})
+
+end_time=$(date +%s)
+duration=$((end_time - start_time))
+formatted_duration=$(printf '%02dh:%02dm:%02ds\n' $(($duration/3600)) $(($duration%3600/60)) $(($duration%60)))
+
+echo "Converted ${NT} (${NT_SIZE}) to ${TTL} (${TTL_SIZE}) in ${formatted_duration}"
